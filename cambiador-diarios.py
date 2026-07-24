ce30cd75842a0bc57c125b873af2769eca0c99bb
fnare <felinaredo@gmail.com>
2026-07-24 19:18:51 -0300

    Add function change_diary

--- cambiador-diarios.py
+++ cambiador-diarios.py
@@ -1,5 +1,11 @@
+def change_diary():
+    decision = input('¿Cual diario quieres cambiar? ')
+
 def main():
     print('Hola este es el editor de diarios')
+
+    decision = change_diary()
+
 
 if __name__ == '__main__':
     main()
